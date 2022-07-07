74488607342ab4d6ee7f6e19a2d66ba8e8a17a3f
Ben Manley <benjamintmanley@gmail.com>
2022-07-07 14:17:05 -0500

    update deps

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+    </Router>
+  </React.StrictMode>
+);
