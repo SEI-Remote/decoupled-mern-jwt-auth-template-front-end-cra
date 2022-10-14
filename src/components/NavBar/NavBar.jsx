d99695a48f64e3a89f71ec0ad925aaeaca98bf03
David Stinson <49245298+DavidStinson@users.noreply.github.com>
2022-10-14 12:42:27 -0500

    clean up code

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,25 +2,21 @@ import { Link } from 'react-router-dom'
 
 const NavBar = ({ user, handleLogout }) => {
   return (
-    <>
+    <nav>
       {user ?
-        <nav>
-          <ul>
-            <li>Welcome, {user.name}</li>
-            <li><Link to="/profiles">Profiles</Link></li>
-            <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
-            <li><Link to="/changePassword">Change Password</Link></li>
-          </ul>
-        </nav>
+        <ul>
+          <li>Welcome, {user.name}</li>
+          <li><Link to="/profiles">Profiles</Link></li>
+          <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
+          <li><Link to="/changePassword">Change Password</Link></li>
+        </ul>
       :
-        <nav>
-          <ul>
-            <li><Link to="/login">Log In</Link></li>
-            <li><Link to="/signup">Sign Up</Link></li>
-          </ul>
-        </nav>
+        <ul>
+          <li><Link to="/login">Log In</Link></li>
+          <li><Link to="/signup">Sign Up</Link></li>
+        </ul>
       }
-    </>
+    </nav>
   )
 }
 
