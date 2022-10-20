4e4bfa4d2b27542c1eb157a513f7d74ae63d28b4
David Stinson <49245298+DavidStinson@users.noreply.github.com>
2022-10-20 14:05:14 -0500

    fix import

--- src/services/profileService.js
+++ src/services/profileService.js
@@ -1,4 +1,4 @@
-import * as tokenService from '../services/tokenService'
+import * as tokenService from './tokenService'
 
 const BASE_URL = `${process.env.REACT_APP_BACK_END_SERVER_URL}/api/profiles`
 
